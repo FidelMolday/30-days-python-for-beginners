9fdcca13f355e1885394a61f77d0b4c64fb794c9
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2024-05-28 06:06:35 +0300

    Add files via upload

---
+++ day1/data type.py
@@ -0,0 +1,8 @@
+print(type(10)) #integer
+print(type(3.14)) #float
+print(type(1+3j)) #complex
+print(type('Fidel')) #string
+print(type([1, 2, 3])) #list
+print(type({'name':'Fidel'})) #dictionary
+print(type({9.8,3.14,2.7})) #set
+print(type((1, 2, 3))) #tuple
