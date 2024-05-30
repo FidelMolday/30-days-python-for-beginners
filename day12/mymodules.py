b011fe72ee8118508927a90fc538f4dadf62d31d
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2024-05-30 22:12:06 +0300

    Add files via upload

---
+++ day12/mymodules.py
@@ -0,0 +1,15 @@
+def generate_full_name(firstname, lastname):
+      space = ' '
+      fullname = firstname + space + lastname
+      return fullname
+
+def sum_two_nums (num1, num2):
+    return num1 + num2
+gravity = 9.81
+person = {
+    "firstname": "Fidel",
+    "age": 20,
+    "country": "Kenya",
+    "city":'Nairobi'
+}
+
